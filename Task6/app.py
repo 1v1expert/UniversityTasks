7c6d7698f1da3c2a4349f7d0c48d2ecb6186d69c
1v1expert <1v1expert@gmail.com>
2019-03-11 01:23:08 +0300

    complete analytic methods

--- Task6/app.py
+++ Task6/app.py
@@ -1,17 +1,37 @@
 import numpy as np
+import json
 
 class Game(object):
 	
 	def __init__(self, **args):
 		#print(args)
-		self.ones = np.ones(1)
-		self.m = args.get('m', None) #columns
-		self.n = args.get('n', None) #lines
-		self.matrix = np.array(args.get('matrix'))
-		print(self.matrix)
+		self.matrix = args.get('matrix', None)
+		self.calculate = args.get('calculate', None)
+		
+		self.n = len(self.matrix) #lines
+		self.ones = np.ones(self.n)
+		self.np_matrix = np.array(self.matrix, dtype=np.float)
+		self.m = self.np_matrix.size / self.n  # columns
+		self.inv_matrix = np.linalg.inv(self.np_matrix)
+		self.optimal_strategy_x, self.optimal_strategy_y = None, None
+		
+		if self.calculate:
+			print(self._get_price_game())
+		
+		#with np.printoptions(precision=3, suppress=True):
+		#	print(vars(self))
+		#print(json.dumps(vars(self), indent=4, sort_keys=True))
+		#print(s)
 	
-	def price_game(self):
-	
+	def _get_price_game(self):
+		self.denominator = np.dot(np.dot(self.ones, self.inv_matrix),  self.ones.transpose())
+		self.optimal_strategy_x = np.dot(self.inv_matrix, self.ones.transpose()) / self.denominator
+		self.optimal_strategy_y = np.dot(self.ones, self.inv_matrix) / self.denominator
+		return self.optimal_strategy_x, self.optimal_strategy_y, 1 / self.denominator
 	
 if __name__ == '__main__':
-	Game(matrix=[[1.4, 5]])
+	matrix = [[2, 1, 3], [3, 0, 1], [1, 2, 1]]
+	#print(len(matrix))
+	Game(matrix=matrix, calculate=True)
+	
+	
